cc4a3e9594d06dbe0f4f9e060716397b458aecfe
tane-git <tane.alexander.wilson@gmail.com>
2022-03-03 21:18:20 +1300

    set config

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,11 +6,46 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/preset-create-react-app"
+    // "@storybook/addon-interactions",
+    // "@storybook/preset-create-react-app"
+    {
+      name: '@storybook/preset-create-react-app',
+      options: {
+        craOverrides: {
+          fileLoaderExludes: ['less'],
+        },
+      },
+    },
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "webpack5"
-  }
-}
+  // "framework": "@storybook/react",
+  // "core": {
+  //   "builder": "webpack5"
+  // },
+  // webpackFinal: async (config, { configType }) => {
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.less$/,
+      use: [
+        "style-loader",
+        "css-loader",
+        {
+          loader: "less-loader",
+          options: {
+            lessOptions: {
+              javascriptEnabled: true,
+            },
+          },
+        },
+      ],
+    });
+    return {
+      ...config,
+      plugins: config.plugins.filter((plugin) => {
+        if (pluging.constructor.name === "ESLintWebpackPlugin") {
+          return false;
+        }
+        return true;
+      }),
+    };
+  },
+};
