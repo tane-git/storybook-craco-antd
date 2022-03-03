d258a9175b651677e882c4545b84833ba300a41f
tane-git <tane.alexander.wilson@gmail.com>
2022-03-03 21:21:14 +1300

    config storybook/preview

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,10 @@
+import '../src/index.less';
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
+  // controls: {
+    // matchers: {
+      // color: /(background|color)$/i,
+      // date: /Date$/,
+    // },
+  // },
 }
